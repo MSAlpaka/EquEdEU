b0c0f3ea13438fb96dd3fc5cb4e1ae5f9b4f6561
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-06-13 14:47:09 +0200

    Add custom exception classes

---
+++ equed-lms/Classes/Exception/EmailException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Equed\EquedLms\Exception;
+
+/**
+ * Represents errors that occur while sending email notifications.
+ */
+final class EmailException extends \RuntimeException
+{
+}
---
+++ equed-lms/Classes/Exception/InvalidFileTypeException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Equed\EquedLms\Exception;
+
+/**
+ * Thrown when an uploaded file does not match the expected type.
+ */
+final class InvalidFileTypeException extends \RuntimeException
+{
+}
