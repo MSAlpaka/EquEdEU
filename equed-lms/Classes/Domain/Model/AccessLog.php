aeb5961724f5220594509149ea4cfbfcd4719d83
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-06-14 14:24:27 +0200

    Remove unused Inject import

--- equed-lms/Classes/Domain/Model/AccessLog.php
+++ equed-lms/Classes/Domain/Model/AccessLog.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Equed\EquedLms\Domain\Model;
 
 use Equed\EquedLms\Domain\Model\Traits\PersistenceTrait;
-use TYPO3\CMS\Extbase\Annotation\Inject;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
 
 /**
