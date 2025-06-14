93ba8781dcfdf98eb94efc2d74dab4303b281431
MSAlpaka <46129705+MSAlpaka@users.noreply.github.com>
2025-06-14 16:49:43 +0200

    feat: add learning path badge query

--- equed-lms/Classes/Domain/Repository/LearningPathRepository.php
+++ equed-lms/Classes/Domain/Repository/LearningPathRepository.php
@@ -6,14 +6,15 @@ namespace Equed\EquedLms\Domain\Repository;
 
 use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
+use Equed\EquedLms\Domain\Model\LearningPath;
 
 /**
  * Repository for LearningPath entities.
-
  *
  * @extends Repository<LearningPath>
  */
-final class LearningPathRepository extends Repository
+
+final class LearningPathRepository extends Repository implements LearningPathRepositoryInterface
 {
     /**
      * Default ordering: first by level, then by title.
@@ -24,4 +25,36 @@ final class LearningPathRepository extends Repository
         'level' => QueryInterface::ORDER_ASCENDING,
         'title' => QueryInterface::ORDER_ASCENDING,
     ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findCompletedWithoutBadge(): array
+    {
+        $qb = $this->createQuery()->getQueryBuilder();
+        $qb
+            ->select('DISTINCT lp.uid')
+            ->from('tx_equedlms_domain_model_learningpath', 'lp')
+            ->join('lp', 'tx_equedlms_domain_model_course', 'c', 'c.learning_path = lp.uid')
+            ->join('c', 'tx_equedlms_domain_model_courseinstance', 'ci', 'ci.courseprogram = c.courseprogram')
+            ->join('ci', 'tx_equedlms_domain_model_usercourserecord', 'ucr', 'ucr.course_instance = ci.uid AND ucr.completed_at IS NOT NULL')
+            ->leftJoin(
+                'lp',
+                'tx_equedlms_domain_model_badgeaward',
+                'ba',
+                "ba.badge_type = 'learning_path' AND ba.badge_code = lp.uid AND ba.frontend_user = ucr.fe_user"
+            )
+            ->where($qb->expr()->isNull('ba.uid'));
+
+        $pathUids = $qb->executeQuery()->fetchFirstColumn();
+
+        if ($pathUids === []) {
+            return [];
+        }
+
+        $query = $this->createQuery();
+        $query->matching($query->in('uid', array_map('intval', $pathUids)));
+
+        return $query->execute()->toArray();
+    }
 }
